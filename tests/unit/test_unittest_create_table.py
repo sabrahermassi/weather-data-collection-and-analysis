fa6720966fb1c972541a99f56db8403bc8921368
sabra <sabra.herm@gmail.com>
2024-06-29 16:26:23 +0200

    attemot to fix unit tests failure on Github Actions

--- tests/unit/test_unittest_create_table.py
+++ tests/unit/test_unittest_create_table.py
@@ -7,7 +7,9 @@ import json
 from http import HTTPStatus
 import os
 from unittest.mock import MagicMock, patch
-sys.path.append('./')
+#sys.path.append('./')
+sys.path.append(os.path.abspath('./src'))
+
 from src.weather_api_data.create_table import load_config, fetch_weather_data
 
 
